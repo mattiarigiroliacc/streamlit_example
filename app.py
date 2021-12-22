3d1917cad77a840c666b36099941871980856209
Mattia Rigiroli <mattia.rigiroli@accenture.com>
2021-12-22 14:36:14 +0100

    update 3

--- app.py
+++ app.py
@@ -5,21 +5,16 @@ import urllib
 def main():
     
     st.set_page_config(layout="wide")
-    readme_text = st.markdown(get_file_content_as_string("instructions.md"))
-
     st.sidebar.title("What to do")
 
     app_mode = st.sidebar.selectbox("Choose the app mode",
         ["Dashboard", "Prediction"])
 
     if app_mode == "Dashboard":
-        readme_text.empty()
         run_dashboard_app()
     elif app_mode == "Prediction":
-        readme_text.empty()
         run_prediction_app()
     
-
 
 def run_prediction_app():
     st.title("Prova App")    
