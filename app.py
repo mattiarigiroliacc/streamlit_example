6f633e5774ab1863990930ef049582e5aa3e69e8
Mattia Rigiroli <mattia.rigiroli@accenture.com>
2021-12-22 14:34:10 +0100

    update

--- app.py
+++ app.py
@@ -9,15 +9,10 @@ def main():
 
     st.sidebar.title("What to do")
 
-
     app_mode = st.sidebar.selectbox("Choose the app mode",
-        ["Instructions", "Show the source code", "Dashboard", "Prediction"])
-    if app_mode == "Instructions":
-        st.sidebar.success('To continue select "Run the app".')
-    elif app_mode == "Show the source code":
-        readme_text.empty()
-        st.code(get_file_content_as_string("app.py"))
-    elif app_mode == "Dashboard":
+        ["Dashboard", "Prediction"])
+
+    if app_mode == "Dashboard":
         readme_text.empty()
         run_dashboard_app()
     elif app_mode == "Prediction":
@@ -32,12 +27,5 @@ def run_prediction_app():
 def run_dashboard_app():
     st.title("Prova App 2")
     
-@st.cache(show_spinner=False)
-def get_file_content_as_string(path):
-    url = 'https://github.com/mattiarigiroliacc/streamlit_example/main/' + path
-    response = urllib.request.urlopen(url)
-    return response.read().decode("utf-8")
-
-
 if __name__ == "__main__":
     main()
